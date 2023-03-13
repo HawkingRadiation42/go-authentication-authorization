c539f0f400ef80e175d221ef245523cf9e70dd1f
HawkingRadiation42 <shrey.209303098@muj.manipal.edu>
2023-03-13 16:21:26 +0530

    intial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"medium/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	public := r.Group("/api")
+	public.POST("/register", controllers.Register)
+	r.Run(":8080")
+
+}
